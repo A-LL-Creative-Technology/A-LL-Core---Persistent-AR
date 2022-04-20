d4e33077ffe584a0f5a18a47bd98aadcced89013
Laurent Rime <laurent@a-ll.tech>
2022-04-20 17:50:42 +0200

    - setup for iOS in progress

---
+++ Runtime/Scripts/WorldMapController.cs
@@ -0,0 +1,21 @@
+#if UNITY_IOS
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldMapController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+#endif
